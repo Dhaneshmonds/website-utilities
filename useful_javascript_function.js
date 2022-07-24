d48d19a476c1aa1bd7d6a30f9bc41dcbe35aef71
Dhaneshmon <dhaneshmon@gmail.com>
2022-07-24 17:14:14 +0530

    Update useful_javascript_function.js

--- useful_javascript_function.js
+++ useful_javascript_function.js
@@ -40,3 +40,8 @@ function escapeHTML(text) {
         return replacements[character];  
     }); 
 }
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
+const dif1 = [1,2,3,4,5,6].diff( [3,4,5] );  
+console.log(dif1); // => [1, 2, 6]
